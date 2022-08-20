3e0306d65ed8edc4aa8be3c3faaf8852ec47b38b
Evgeny Kapranov <e.s.kapranov@yandex.ru>
2022-08-20 14:52:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import sqlalchemy
+import json
+
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+from models import create_tables, Stock, Book, Publisher, Sale, Shop
+
+def search_publisher(session, name=None, id=None):
+    if id is not None:
+        result = session.query(Publisher).filter(Publisher.id == id).all()
+        return result
+
+    if name is not None:
+        result = []
+        for c in session.query(Publisher).filter(Publisher.name == name).all():
+            result.append(c)
+        return result
+
+def data_loader(session):
+    with open('test_data.json', 'r') as fd:
+        data = json.load(fd)
+
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+        session.add(model(id=record.get('pk'), **record.get('fields')))
+    session.commit()
+
+
+DSN = "postgresql://postgres:postgres@localhost:5432/netology_orm"
+engine = sqlalchemy.create_engine(DSN)
+
+# create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# data_loader(session)
+requ = input('Введите ID или название автора:')
+if requ.isdigit():
+    print(*search_publisher(session, id=requ))
+else:
+    print(*search_publisher(session, name=requ))
+
+# for c in session.query(Publisher).filter(Publisher.id == 1).all():
+#     print(c)
+
+session.close()
+
+
---
+++ models.py
@@ -0,0 +1,57 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Publisher(Base):
+    __tablename__ = "Publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+class Book(Base):
+    __tablename__ = "Book"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=40), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("Publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref="books")
+
+class Shop(Base):
+    __tablename__ = "Shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+class Stock(Base):
+    __tablename__ = "Stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("Book.id"),nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("Shop.id"),nullable=False)
+    count = sq.Column(sq.Integer)
+
+    book = relationship(Book, backref="Stock")
+    shop = relationship(Shop, backref="Stock")
+
+class Sale(Base):
+    __tablename__ = "Sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float)
+    date_sale = sq.Column(sq.Date)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("Stock.id"), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    stock = relationship(Stock, backref="Sale")
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+
